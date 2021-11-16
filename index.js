f2af6bd52fefb510aff5280c75feddbc0254d3fd
stef <stephenegbert@gmail.com>
2021-11-16 14:05:09 -0500

    remove potential env for port to avoid mixups

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ app.get(`/reviews`, cors(), async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 4000, () => {
+app.listen(4000, () => {
   console.log("Server is listening on port 4000");
 });
